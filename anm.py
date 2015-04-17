c182287724edbc7ad622857cba12f2386b555acf
me <me@no.com>
2015-04-17 14:38:42 +0800

    duplicate assets removed

--- anm.py
+++ anm.py
@@ -13,7 +13,6 @@ class anm_asset:
     def get_props(asset):
         # init a dictionary for whatever properties we want
         asset_num = str(asset.asset_num)
-        print "defining props for", asset_num
         # set path for the file of the page for this asset 
         asset_page_file = "./asset_pages/"+str(asset_num)+".html"
         # make the folder for asset page files if it doesn't exist
@@ -24,24 +23,33 @@ class anm_asset:
         if not os.path.exists(asset_page_file):
             asset_page_url = "http://ofa.arkib.gov.my/ofa/group/asset/"+str(asset_num)
             print "Fetching", asset_page_url 
-            asset_page = requests.get(asset_page_url)
-            print asset_page
+            try:
+                asset_page = requests.get(asset_page_url, timeout=30)
+                return asset_page
+            except requests.exceptions.Timeout:
+                print(asset_page_url,'Timed Out!')
+            except requests.exceptions.ConnectionError:
+                print(asset_page_url,'Connection Error!')
             with open(asset_page_file, "wb") as file:
                 file.write(asset_page.text)
         # read the asset page file into BS
-        print "reading", asset_page_file 
-        asset_page = BS(open(asset_page_file))
-        # get the URL of the asset PDF
-        iframe_src = asset_page.find('iframe')['src']
-        if "pdf" in iframe_src:
-            asset.props["pdf_url"] = re.search("(?P<url>http?://[^\s]+)", iframe_src).group("url")
+        print "Attempting to read", asset_page_file
+        try:
+            asset_page = BS(open(asset_page_file))
+            # get the URL of the asset PDF
+            iframe_src = asset_page.find('iframe')['src']
+            if "pdf" in iframe_src:
+                asset.props["pdf_url"] = re.search("(?P<url>http?://[^\s]+)", iframe_src).group("url")
         # get the table with the other properties
         #table_non = BS(asset_page.find_all('table', {'class' : "non"}))
         #table_non_rows = table_non.find_all('tr')
         # COME BACK TO THIS
+        except:
+            print "Failed to define properties for asset", asset.asset_num
         return asset
 
 def get_all_asset_nums(search_url):
+    result_list = []
     asset_list = []
     if not "http://" in search_url:
         search_page = open(search_url)
@@ -50,35 +58,39 @@ def get_all_asset_nums(search_url):
     search_page = BS(search_page)
     hrefs = search_page.find_all(href=re.compile("asset"))
     for result in hrefs:
-        asset_list.append((re.findall(r'\d+', str(result))))
-    return asset_list
+        result_nums = re.findall(r'\d+', str(result))
+        if len(result_nums[0]) > 4:
+            asset_list.append(result_nums[0])
+    return sorted(dedupe(asset_list))
 
-# ('div', {'class' : 'category-box-item-title-02'}):
-#         box = BS(box)
-#         print box.find_all('a')['href']
-        # for result in hrefs:
-        #     assetlist.append(re.findall(r'\d+', hrefs))
-    # print asset_list
-    # return asset_list
+# fastest according to  http://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-in-python-whilst-preserving-order
+def dedupe(num_list):
+    seen = set()
+    seen_add = seen.add
+    return [ x for x in num_list if not (x in seen or seen_add(x))]
 
 search_url = "oil_palm_search.html"
 asset_num_list = get_all_asset_nums(search_url)
 for asset_num in asset_num_list:
-    asset = anm_asset(asset_num[0])
+    asset = anm_asset(asset_num)
     if len(asset.asset_num) > 5:
-        print "Getting properties for asset", asset.asset_num
         asset.get_props()
-#    print asset.asset_num, asset.props
         pdf_dest = "./pdf/" + str(asset.asset_num) + ".pdf"
         if os.path.exists(pdf_dest):
             print pdf_dest, "already exists"
         elif 'pdf_url' in asset.props:
-            print "Downloading PDF from", asset.props['pdf_url']
-            rq = urllib2.Request(asset.props['pdf_url'])
-            res = urllib2.urlopen(rq)
-            pdf = open("./pdf/" + str(asset.asset_num) + ".pdf", 'wb')
-            pdf.write(res.read())
-            pdf.close()
+            if not os.path.exists("./pdf/" + str(asset.asset_num) + ".pdf"):
+                print "Downloading PDF from", asset.props['pdf_url'], "to ./pdf/"+ str(asset.asset_num)+".pdf" 
+                try:
+                    rq = urllib2.Request(asset.props['pdf_url'])
+                    res = urllib2.urlopen(rq)
+                    pdf = open("./pdf/" + str(asset.asset_num) + ".pdf", 'wb')
+                    pdf.write(res.read())
+                    pdf.close()
+                except:
+                    print "Failure to download", pdf_url
+            else: 
+                print "./pdf/" + str(asset.asset_num) + ".pdf already exists!"
 
 #         self.title = 
 #         # No Penerimaan
